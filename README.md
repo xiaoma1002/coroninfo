c42f4490a673786aa8c6f93c7fe2441a22b0ea10
ym474 <ym474@cornell.edu>
2020-03-21 23:03:20 -0400

    interaction: text, zoom

---
+++ README.md
@@ -0,0 +1,1 @@
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Qw6uAg3EO64?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-import { select, json, geoPath, geoNaturalEarth1  } from 'd3';
+import { select, json, geoPath, geoNaturalEarth1, zoom, event } from 'd3';
 import { feature } from 'topojson';
 const svg = select('svg');
 
 const projection = geoNaturalEarth1();
 const pathGenerator = geoPath().projection(projection);
 
-
-svg.append('path')
+const g = svg.append('g');
+g.append('path')
 	.attr('d', pathGenerator({type: "Sphere"}))
 	.attr('class','Sphere');
 
+svg.call(zoom().on('zoom',()=>{
+	g.attr('transform', event.transform)
+}));
+// svg.call(zoom().on('zoom',() => {
+// 	console.log('zoom');
+// });
 
-json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json')
 	.then(data => {
   	const countries = feature(data, data.objects.countries);
   	console.log(countries);
   
-  	svg.selectAll('path')
+  	g.selectAll('path')
       .data(countries.features)
       .enter().append('path')// we use path in css
-      .attr('d', pathGenerator);
+        .attr('d', pathGenerator)
+        .attr('class', 'country')
+  		.append('title')
+  			.text(d=>d.properties.name);
 	});
